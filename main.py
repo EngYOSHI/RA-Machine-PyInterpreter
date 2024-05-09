62891aa2c055569c166163289cfc7006b3fa3e2d
EngYOSHI <243426029@ccmailg.meijo-u.ac.jp>
2024-05-09 23:02:31 +0900

    fix: ラベル名に'-'と'_'を追加で使えるようにした
    ・コメント除去後とパース後のそれぞれで，異なる文字種チェックを実行するようにした。

--- main.py
+++ main.py
@@ -42,10 +42,11 @@ def procArg():
 
 def run(prog, watch, step):
   prog = removeComment(prog)
-  charCheck(prog)
+  charCheck1(prog) #コメントを除去した後に文字種チェック
   label, prog = getlabel(prog) #ラベルと行数の対応付け
   prog = removeIndent(prog)
   prog = parseProg(prog)
+  charCheck2(prog) #リストにパースした後に文字種チェック
 
   count = 0
   mem = {} #メモリ効率をよくするため辞書型。リスト型は大きい番地を使用した際に効率が悪い
@@ -181,13 +182,28 @@ def removeComment(prog):
     printProg(prog)
   return prog
   
-def charCheck(prog):
-  #文字種チェック
-  p = re.compile('[a-zA-Z0-9*=,: \n\t]+')
+def charCheck1(prog):
+  #コメント除去後の文字種チェック
+  p = re.compile('[-_:,=*a-zA-Z0-9 \n\t]+')
   for i, l in enumerate(prog):
     if not p.fullmatch(l):
       err("SyntaxError", "Unexpected character in line " + str(i + 1))
-  dbg("charCheck Finished")
+  dbg("charCheck1 Finished")
+
+
+def charCheck2(prog):
+  #リストにパースした後の文字種チェック
+  p = re.compile('[=*a-zA-Z0-9]+')
+  for i, l in enumerate(prog):
+    if l[0] == '' or l[0] == 'HALT' or l[0] == 'JUMP' or l[0] == 'JGTZ' or l[0] == 'JZERO':
+      continue
+    elif l[0] == 'SJ':
+      if not (p.fullmatch(l[1]) and p.fullmatch(l[2])):
+        err("SyntaxError", "Unexpected character in line " + str(i + 1))
+    else:
+      if not (p.fullmatch(l[1])):
+        err("SyntaxError", "Unexpected character in line " + str(i + 1))
+  dbg("charCheck2 Finished")
 
 
 def getlabel(prog):
